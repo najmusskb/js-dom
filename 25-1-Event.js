d432bf0a43901cf4bbfa51b6cb6541ad6a794ed4
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-08-14 20:16:17 +0600

    add even listener

--- 25-1-Event.js
+++ 25-1-Event.js
@@ -37,3 +37,37 @@ MakePurple.onclick = function makepurplecolor() {
 
 
 
+
+
+//  option 05 Addeventlistener
+const pinkButton = document.getElementById('make-pink');
+
+pinkButton.addEventListener('click', makePink);
+
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
+
+}
+
+
+
+
+// option - 06 (vvi)
+
+const tometoButton = document.getElementById('make-tometo')
+tometoButton.addEventListener('click', function maketometo() {
+    document.body.style.backgroundColor = 'tomato';
+})
+
+
+
+
+
+//  option 7 Final  (eitai amra use korbo )
+
+const GoldenRod = document.getElementById('make-goldenrod').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'goldenrod';
+})
+
+
+
